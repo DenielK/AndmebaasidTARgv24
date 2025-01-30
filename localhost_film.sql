da5d3482d613f96054e509ec41afe4d544466a97
DenielK <denielkruusman@gmail.com>
2025-01-30 10:35:48 +0200

    Update localhost_film.sql

--- localhost_film.sql
+++ localhost_film.sql
@@ -1,6 +1,17 @@
 Create table film(
-    filmID int PRIMARY KEY identity(1,1),
+    filmID int PRIMARY KEY AUTO_INCREMENT,
     filmNimetus varchar(30) not null,
     aasta int,
     eelarveHind decimal(7.1)
-    Select*from film
+    
+
+Insert into film(filmNimetus,aasta,eelarveHind)
+    values
+
+Create TABLE zanr(
+    zanrID int primary key auto_increment,
+    zanrNimetus varchar(20) unique
+    );
+
+insert into zanr(zanrNimetus)
+values ('draama'),('detektiiv')
