31e4a7653220556c6f10383465ebeede71223782
DenielK <denielkruusman@gmail.com>
2025-04-10 09:37:07 +0300

    Create tabel.sql

---
+++ tabel.sql
@@ -0,0 +1,7 @@
+create table lehed(
+                      id int not null AUTO_INCREMENT PRIMARY KEY,
+                      pealkiri varchar(50),
+                      sisu text);
+
+insert into lehed(pealkiri, sisu)
+VALUES('Ilm on külm','Aprill on käes');
